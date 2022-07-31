8cf88dbbd605a1be6e27e5d1c7572272bbf6fdb7
Gustavo Reis Silva <gustavoreissilva.2016@gmail.com>
2022-07-31 10:48:44 -0300

    feat(front-end) add initial configs

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-500 to-blue-500">
+     hello
+    </div>
+  )
+}
+
+export default Home
